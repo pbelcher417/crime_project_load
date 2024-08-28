b26fe564b82ba43562f0d930be5e9491fe15c170
Paul_Belcher <pbelcher417@gmail.com>
2024-08-28 18:38:35 +0100

    Initialized dlt

---
+++ open_crime_load.py
@@ -0,0 +1,18 @@
+import dlt
+from dlt.sources.helpers import requests
+
+# Specify the URL of the API endpoint
+url = "https://data.police.uk/api/forces"
+# Make a request and check if it was successful
+response = requests.get(url)
+response.raise_for_status()
+
+pipeline = dlt.pipeline(
+    pipeline_name="github_issues",
+    destination="duckdb",
+    dataset_name="github_data",
+)
+# The response contains a list of issues
+load_info = pipeline.run(response.json(), table_name="issues")
+
+print(load_info)
